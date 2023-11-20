af16559d9249c20e82eafbdbcc85a3403b347921
Honza <honzik.mraz@gmail.com>
2023-11-20 10:43:00 +0100

    DTOin nevrací chyby

--- dist/utils/validation.js
+++ dist/utils/validation.js
@@ -4,6 +4,8 @@ const express_validator_1 = require("express-validator");
 const validate = (req, res) => {
     const result = (0, express_validator_1.validationResult)(req);
     if (!result.isEmpty()) {
+        //Re: Validace nevrací chyby (co je na dtoIn chybné).
+        //Pole Result obsahuje všechny chyby, které v dtoIn byly oproti schématu. https://express-validator.github.io/docs/api/validation-result
         res.status(400).json({ errors: result.array() });
         return false;
     }
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -4,6 +4,9 @@ import { IReq, IRes } from "./type";
 const validate = (req: IReq<any>, res: IRes) => {
   const result = validationResult(req);
   if (!result.isEmpty()) {
+    //Re: Validace nevrací chyby (co je na dtoIn chybné).
+    //Pole Result obsahuje všechny chyby, které v dtoIn byly oproti schématu. https://express-validator.github.io/docs/api/validation-result
+    //Takže si nejsem úplně jistý, kde byl problém.
     res.status(400).json({ errors: result.array() });
     return false;
   }
